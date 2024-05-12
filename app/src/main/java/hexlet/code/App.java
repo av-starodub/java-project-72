abb85197f70e9f3911815b5a643bc393dbf4e076
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-05-12 03:20:27 +0300

    Implement the application to work with HikariCp and Flyway

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,43 +1,56 @@
 package hexlet.code;
 
 import hexlet.code.controllers.RootController;
+import hexlet.code.repository.AbstractDao;
 import io.javalin.Javalin;
+import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class App {
+
+    public static final Logger log = LoggerFactory.getLogger(App.class);
     private static final String DEFAULT_PORT = "7070";
+
     private App() {
     }
 
     public static void main(String[] args) {
-        Javalin app = getApp();
-        app.start(getPort());
+        flywayMigrate();
+        var app = getApp();
+        var port = getPort();
+        app.start(port);
     }
 
-    private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", DEFAULT_PORT);
-        return Integer.parseInt(port);
-    }
-
-    private static String getMode() {
-        return System.getenv().getOrDefault("APP_ENV", "development");
-    }
-
-    private static boolean isProduction() {
-        return getMode().equals("production");
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            if (!"production".equals(System.getenv().get("APP_MODE"))) {
+                config.bundledPlugins.enableDevLogging();
+                log.info("enable dev logging ");
+            }
+        });
+        addRoutes(app);
+        app.before(ctx -> ctx.attribute("ctx", ctx));
+        return app;
     }
 
     private static void addRoutes(Javalin app) {
         app.get("/", RootController.welcome);
     }
 
-    public static Javalin getApp() {
-        Javalin app = Javalin.create(config -> {
-            if (!isProduction()) {
-                config.bundledPlugins.enableDevLogging();
-            }
-        });
-        addRoutes(app);
-        app.before(ctx -> ctx.attribute("ctx", ctx));
-        return app;
+    private static int getPort() {
+        var port = System.getenv().getOrDefault("PORT", DEFAULT_PORT);
+        return Integer.parseInt(port);
+    }
+
+    private static void flywayMigrate() {
+        log.info("db migration started...");
+        var flyway = Flyway.configure()
+                .dataSource(AbstractDao.DATASOURCE)
+                .locations("classpath:/db/migration")
+                .load();
+        flyway.migrate();
+        log.info("db migration finished.");
+        log.info("***");
     }
 }
---
+++ app/src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,5 @@
+create table urls (
+    id         bigint not null primary key,
+    name       varchar(255),
+    created_at timestamp not null
+);
