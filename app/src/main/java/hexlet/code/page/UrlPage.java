4e142ef468d56a43230cf8160f8f2645ffa0d8ae
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-12-03 02:45:30 +0300

    Implement handler test for POST /urls/{id}/checks

--- app/src/main/java/hexlet/code/page/UrlPage.java
+++ app/src/main/java/hexlet/code/page/UrlPage.java
@@ -4,12 +4,10 @@ import hexlet.code.model.Url;
 import hexlet.code.model.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public final class UrlPage extends BasePage {
 
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -2,16 +2,21 @@ package hexlet.code;
 
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
-import org.junit.jupiter.api.AfterEach;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static java.util.Objects.nonNull;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class AppTest {
+
     private static final int OK_STATUS_CODE = 200;
+
     private static final int NOT_FOUND_STATUS_CODE = 404;
 
     private Javalin app;
@@ -19,11 +24,6 @@ class AppTest {
     @BeforeEach
     void setUp() {
         app = App.getApp();
-    }
-
-    @AfterEach
-    void tearDown() {
-        app.stop();
     }
 
     @Test
@@ -75,5 +75,41 @@ class AppTest {
             var response = client.get("/urls/11111");
             assertThat(response.code()).isEqualTo(NOT_FOUND_STATUS_CODE);
         });
+    }
+
+    @Test
+    @DisplayName("Should handle POST /urls/{id}/checks with success check correctly")
+    void checkSuccessUrlCheck() throws IOException {
+        var mockWebServer = new MockWebServer();
+        mockWebServer.start();
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(OK_STATUS_CODE)
+                .setBody("""
+                        <html>
+                            <head>
+                                <title>Test Page</title>
+                                <meta name="description" content="Test Description">
+                            </head>
+                            <body>
+                                <h1>Test H1</h1>
+                            </body>
+                        </html>
+                        """));
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=" + mockWebServer.url("/");
+            var postResponse = client.post("/urls", requestBody);
+            assertThat(postResponse.code()).isEqualTo(OK_STATUS_CODE);
+
+            var checkResponse = client.post("/urls/1/checks");
+            assertThat(checkResponse.code()).isEqualTo(OK_STATUS_CODE);
+
+            var body = checkResponse.body();
+            assertThat(body).isNotNull();
+            assertThat(body.string())
+                    .contains("Test Page")
+                    .contains("Test Description")
+                    .contains("Test H1");
+        });
+        mockWebServer.shutdown();
     }
 }
