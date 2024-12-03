27c5ec4942678c3ed21cbfbfaf2ca85bfefb08af
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-12-03 07:10:10 +0300

    Add schema.sql

---
+++ app/src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS url_checks;
+DROP TABLE IF EXISTS urls;
+
+CREATE TABLE urls
+(
+    id         bigint GENERATED ALWAYS AS IDENTITY,
+    name       varchar(255) NOT NULL,
+    created_at timestamp    NOT NULL,
+    CONSTRAINT pk_url PRIMARY KEY (id)
+);
+
+CREATE TABLE url_checks
+(
+    id          bigint GENERATED ALWAYS AS IDENTITY,
+    url_id      bigint    NOT NULL,
+    status_code integer   NOT NULL,
+    created_at  timestamp NOT NULL,
+    title       varchar(255),
+    h1          varchar(255),
+    description text,
+    CONSTRAINT pk_url_check PRIMARY KEY (id),
+    CONSTRAINT fk_url_checks_url_id FOREIGN KEY (url_id) REFERENCES urls(id)
+);
